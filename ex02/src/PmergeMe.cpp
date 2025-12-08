e3683afba9bd1c4feea224097061b2035919ace4
LouAntonio <lourencodala88@gmail.com>
2025-12-08 12:05:43 +0100

    parseInput method and isNumeric aux funtion used to validate input

--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -6,7 +6,25 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 10:44:50 by lantonio          #+#    #+#             */
-/*   Updated: 2025/12/08 10:44:56 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/12/08 11:41:57 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/PmergeMe.hpp"
+
+void	PmergeMe::parseInput(char **av) {
+	std::string	token;
+	int			value;
+
+	while (*av) {
+		if (isNumeric(*av)) {
+			value = std::atoi(*av);
+			if (value < 0)
+				std::cout << "Negative number " << value << std::endl;
+			else
+				std::cout << "Number " << value << std::endl;
+		} else
+			std::cout << "Invalid token = " << *av << std::endl;
+		av++;
+	}
+}
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,21 +6,34 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 10:44:44 by lantonio          #+#    #+#             */
-/*   Updated: 2025/12/08 10:59:06 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/12/08 11:48:56 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/PmergeMe.hpp"
 
+bool isNumeric(const std::string str)
+{
+	size_t	i = 0;
+	if (str.empty())
+		return false;
+	if (str[i] == '-' || str[i] == '+') i = 1;
+	if (str[i] == '\0') return false;
+	for (; i < str.size(); ++i)
+		if (!std::isdigit(str[i]))
+			return false;
+	return true;
+}
+
 int main(int ac, char **av) {
 	if (ac > 2) {
 		try {
-			std::cout << "OK" << std::endl;
+			PmergeMe	pmergeme;
+			pmergeme.parseInput(av + 1);
 		} catch (std::exception &e) {
 			std::cerr << "Error: " << e.what() << std::endl;
 		}
 	} else
 		std::cout << "./PmergeMe [args]" << std::endl;
-	(void)av;
 	return 0;
 }
