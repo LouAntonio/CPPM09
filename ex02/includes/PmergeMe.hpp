24f5f4f951651a9ea038848490f8eb5e2fa945a2
LouAntonio <lourencodala88@gmail.com>
2025-12-08 12:04:24 +0100

    using deque and vector | update asignment constructors to match new vars

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 10:45:06 by lantonio          #+#    #+#             */
-/*   Updated: 2025/12/08 11:08:57 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/12/08 12:00:17 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,30 @@
 
 #include <iostream>
 #include <exception>
+#include <sstream>
+#include <cstdlib>
 #include <string>
 #include <algorithm>
+#include <deque>
+#include <vector>
+
+bool	isNumeric(const std::string str);
 
 class PmergeMe {
+	private:
+		std::vector<int>	vet;
+		std::deque<int>		deq;
 	public:
 		PmergeMe() {};
-		PmergeMe(const PmergeMe &src) {};
+		PmergeMe(const PmergeMe &src) { *this = src; };
 		PmergeMe &operator=(const PmergeMe &src) {
 			if (this != &src)
-				*this = src;
+				(this->vet = src.vet, this->deq = src.deq);
 			return *this;
 		}
-		~PmergeMe();
+		~PmergeMe() {};
 
-		void	parseInput(std::string input);
+		void	parseInput(char **av);
 };
 
 #endif
