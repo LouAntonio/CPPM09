accfd78cabeefa05edfe6670dbcc76b9ebaf53f6
LouAntonio <lourencodala88@gmail.com>
2025-12-12 10:58:52 +0100

    program output as required by the subject

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 10:44:44 by lantonio          #+#    #+#             */
-/*   Updated: 2025/12/12 10:54:33 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/12/12 10:57:09 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,20 +48,15 @@ int main(int ac, char **av) {
 			pmergeme.printInput(av + 1);
 			std::cout << "After:  ";
 			pmergeme.printContainer(pmergeme.ordered_vet);
-			std::cout << "Time to process a range of " << pmergeme.len << " elements " << total << " ms" << std::endl;
+			std::cout << "Time to process a range of " << pmergeme.len << " elements with std::vector " << total << " ms" << std::endl;
 			
-			std::cout << "---" << std::endl;
 			gettimeofday(&start, NULL);
 			pmergeme.sortDeq(pmergeme.original_deq);
 			gettimeofday(&end, NULL);
 			seconds = end.tv_sec  - start.tv_sec;
 			usec = end.tv_usec - start.tv_usec;
 			total = seconds * 1000000 + usec;
-			std::cout << "Before: ";
-			pmergeme.printInput(av + 1);
-			std::cout << "After:  ";
-			pmergeme.printContainer(pmergeme.ordered_deq);
-			std::cout << "Time to process a range of " << pmergeme.len << " elements " << total << " ms" << std::endl;
+			std::cout << "Time to process a range of " << pmergeme.len << " elements with std::dequee " << total << " ms" << std::endl;
 		} catch (std::exception &e) {
 			std::cerr << "Error: " << e.what() << std::endl;
 		}
